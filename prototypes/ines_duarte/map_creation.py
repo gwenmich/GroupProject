719805f7bd272cc9867ea7412094230764d95ba4
Ines Duarte <164347704+inesdcd@users.noreply.github.com>
2024-11-17 12:23:00 +0000

    moved assets into maps_assets folder

--- prototypes/ines_duarte/map_creation.py
+++ prototypes/ines_duarte/map_creation.py
@@ -9,40 +9,40 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # setting the title on game window
 pygame.display.set_caption("Thesis Quest")
 # setting icon on game window
-game_icon = pygame.image.load('school_2.png')
+game_icon = pygame.image.load('map_assets/school_2.png')
 pygame.display.set_icon(game_icon)
 
 
 # loading building, grass and wall images and scaling them
 # size in map_config file
-building_image_1 = pygame.image.load('police-station.png')
+building_image_1 = pygame.image.load('map_assets/police-station.png')
 building_image_1 = pygame.transform.scale(building_image_1, (BUILDING_SIZE, BUILDING_SIZE))
 
-building_image_2 = pygame.image.load('royal-palace.png')
+building_image_2 = pygame.image.load('map_assets/royal-palace.png')
 building_image_2 = pygame.transform.scale(building_image_2, (BUILDING_SIZE, BUILDING_SIZE))
 
-building_image_3 = pygame.image.load('school.png')
+building_image_3 = pygame.image.load('map_assets/school.png')
 building_image_3 = pygame.transform.scale(building_image_3, (BUILDING_SIZE, BUILDING_SIZE))
 
-building_image_4 = pygame.image.load('apartment.png')
+building_image_4 = pygame.image.load('map_assets/apartment.png')
 building_image_4 = pygame.transform.scale(building_image_4, (BUILDING_SIZE, BUILDING_SIZE))
 
-building_image_5 = pygame.image.load('museum.png')
+building_image_5 = pygame.image.load('map_assets/museum.png')
 building_image_5 = pygame.transform.scale(building_image_5, (BUILDING_SIZE, BUILDING_SIZE))
 
-grass_image = pygame.image.load('grass.png')
+grass_image = pygame.image.load('map_assets/grass.png')
 grass_image = pygame.transform.scale(grass_image, (GRASS_SIZE, GRASS_SIZE))
 
-tree_image_1 = pygame.image.load('sakura.png')
+tree_image_1 = pygame.image.load('map_assets/sakura.png')
 tree_image_1 = pygame.transform.scale(tree_image_1, (TREE_SIZE, TREE_SIZE))
 
-tree_image_2 = pygame.image.load('tree.png')
+tree_image_2 = pygame.image.load('map_assets/tree.png')
 tree_image_2 = pygame.transform.scale(tree_image_2, (TREE_SIZE, TREE_SIZE))
 
-bench_image = pygame.image.load('bench.png')
+bench_image = pygame.image.load('map_assets/bench.png')
 bench_image = pygame.transform.scale(bench_image, (BENCH_SIZE, BENCH_SIZE))
 
-wall_image = pygame.image.load('wall_1.png')
+wall_image = pygame.image.load('map_assets/wall_1.png')
 wall_image = pygame.transform.scale(wall_image, (WALL_SIZE, WALL_SIZE))
 
 
