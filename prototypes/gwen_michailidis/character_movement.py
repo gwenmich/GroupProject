8ae9efc1d1b0459055e4bbe2f6fcb73e42fdc714
gwenmich <79330147+cgmich@users.noreply.github.com>
2024-11-23 18:38:50 +0000

    Completed sprite animation and added spritesheet

--- prototypes/gwen_michailidis/character_movement.py
+++ prototypes/gwen_michailidis/character_movement.py
@@ -2,24 +2,23 @@ import pygame
 from config import *
 from bars_classes import StressBar, GamesBar
 from timer import Timer
+from spritesheet import SpriteSheet
 
 # from character_building_collision import enter_building
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Character movement")
+
 clock = pygame.time.Clock()
 
 dt = 0
 
 # position player in the centre of the screen
 player_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+character_rect = pygame.Rect(player_position.x, player_position.y, 64, 64)
 
-# load main character image and get its rectangle
-character = pygame.image.load('girl64.png').convert_alpha()
-x = character.get_width()
-y = character.get_height()
+
 
 # initialise stress and games bars
 stress_bar = StressBar(900, 20, 70, 16, 100)
@@ -29,6 +28,22 @@ games_bar = GamesBar(510, 20, 70, 16, 4)
 timer = Timer(1800)
 # creating timer where a userevent is posted to event queue every 1 second (1000 milliseconds)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
+
+# lines 33 - 46 will be going into a function or class
+# load spritesheet
+character_sprite_image = pygame.image.load('girl_sprite.png').convert_alpha()
+character_sprite = SpriteSheet(character_sprite_image)
+bg_colour = "#ff00d6"
+
+#
+animation_list = []
+animation_steps = 2
+last_animation_update = pygame.time.get_ticks()
+animation_cooldown = 300
+frame = 0
+
+for x in range(animation_steps):
+    animation_list.append(character_sprite.get_image(x, 64, 64, bg_colour))
 
 
 running = True
@@ -41,17 +56,28 @@ while running:
     # background screen colour - to change with map
     screen.fill((58, 179, 66))
 
+    # lines 59 - 73 will be going in a function or class
+    # sync player position with rect
+    character_rect.topleft = player_position
+
+    # update animation frame
+    current_animation_time = pygame.time.get_ticks()
+    if current_animation_time - last_animation_update >= animation_cooldown:
+        frame += 1
+        last_animation_update = current_animation_time
+        if frame >= len(animation_list):
+            frame = 0
+
+
+    # draw animated sprite on screen
+    screen.blit(animation_list[frame], character_rect.topleft)
+
+
     # add stress and games bar to screen
     stress_bar.draw(screen)
     stress_bar.draw_text(screen)
     games_bar.draw(screen)
     games_bar.draw_text(screen)
-
-    # position character's rect in the centre of the screen
-    character_rect = character.get_rect(center=player_position)
-
-    # adding character image to centre of screen
-    screen.blit(character, character_rect)
 
 
     # get status of keys on keyboard
@@ -60,11 +86,11 @@ while running:
     # movement of character
     if keyboard[pygame.K_UP] and character_rect.y > 0:
         player_position.y -= 250 * dt
-    if keyboard[pygame.K_DOWN] and character_rect.y < SCREEN_HEIGHT - y:
+    if keyboard[pygame.K_DOWN] and character_rect.y < SCREEN_HEIGHT - 64:
         player_position.y += 250 * dt
     if keyboard[pygame.K_LEFT] and character_rect.x > 0:
         player_position.x -= 250 * dt
-    if keyboard[pygame.K_RIGHT] and character_rect.x < SCREEN_WIDTH - x:
+    if keyboard[pygame.K_RIGHT] and character_rect.x < SCREEN_WIDTH - 64:
         player_position.x += 250 * dt
 
 
@@ -81,7 +107,8 @@ while running:
                 running = False
         # decreases timer by 1 second every second
         if event.type == pygame.USEREVENT:
-                timer.time_limit -= 1
+                timer.timer_duration -= 1
+
 
     timer.countdown(screen)
 
