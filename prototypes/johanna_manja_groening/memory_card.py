616af784512e829a177e93ffc63a2edd6f4fa5d8
Johoneyx <johanna.m.groening@gmail.com>
2024-11-25 07:17:03 +0000

    create folders for repo, copy over all assets and code done so far

---
+++ prototypes/johanna_manja_groening/memory_card.py
@@ -0,0 +1,22 @@
+
+class Card(pygame.sprite.Sprite):
+    def __init__(self, filename, x, y):
+        super().__init__()
+
+        self.name = filename.split('.')[0]
+
+        self.original_image = pygame.image.load('images/' + filename)
+
+        self.back_image = pygame.image.load('images/affirmations')
+
+        self.image = self.back_image
+        self.shown = False
+
+    def update(self):
+        self.image = self.original_image if self.shown else self.back_image
+
+    def show(self):
+        self.shown = True
+
+    def hide(self):
+        self.shown = False
