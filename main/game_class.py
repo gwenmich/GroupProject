7870edcfae68149f7afb959d8ca0e6e127cf7451
gwenmich <79330147+cgmich@users.noreply.github.com>
2024-12-01 13:15:32 +0000

    Group integration menu and map

--- main/game_class.py
+++ main/game_class.py
@@ -1,19 +1,20 @@
 from character_class import Character
 from main_config import FPS
-from world.game_screen_classes import Screen, MapScreen
+from world.game_screen_classes import MapScreen
 import pygame
+from menu_class import Menu
+import sys
 
 class Game:
     def __init__(self):
         # self.manager = manager
-        self.screen = MapScreen()
-        self.player = Character(self.screen.screen, "assets/sprites/girl_sprite.png", 2, "#ff00d6", 64, 64)
+        self.map_screen = MapScreen()
+        self.menu = Menu()
+        self.player = Character(self.map_screen.screen, "assets/sprites/girl_sprite.png", 2, "#ff00d6", 64, 64)
         self.clock = pygame.time.Clock()
         self.dt = 0
         self.game_state = "Main Menu"
-        self.selected_option = 0
-        self.menu = Menu(self)
-        self.game = Game(self)
+        # self.game = Game(self)
         self.setup_music()
 
 
@@ -22,8 +23,11 @@ class Game:
         pygame.mixer.music.play(-1)
 
 
-    def run(self):
+
+    def loop(self):
         while True:
+            self.dt = self.clock.tick(FPS)/1000
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -35,37 +39,21 @@ class Game:
                 #     self.game.handle_input(event)
 
             if self.game_state == "Main Menu":
-                self.menu.display()
+                self.menu.display(self.map_screen.screen)
+                self.menu.handle_input()
             elif self.game_state == "Map":
-                self.game.loop()
+                self.map_screen.draw()
+                self.player.animate(self.map_screen.screen)
+                self.player.move(250, self.dt)
 
-            self.clock.tick(FPS)
+            # pygame.mixer.music.load('assets/main_menu/mapmusic.mp3')
+            # pygame.mixer.music.play(-1)
+            pygame.display.flip()
 
-    # def handle_input(self, event):
-    #     pass
-
-
-
-
-    def loop(self):
-
-        self.dt = self.clock.tick(FPS)/1000
-
-
-        # self.manager.screen.fill((0, 200, 110))
-        # need function to draw map
-
-        self.player.animate(self.screen)
-        self.player.move(250, self.dt)
-
-        pygame.display.flip()
-
-
-
+        # def handle_input(self, event):
+        #     pass
 
 
 if __name__ == "__main__":
-
     game = Game()
-
     game.loop()
