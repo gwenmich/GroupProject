bfeb55ae08b1aa62cc135cb3c41f2f103d2f62e6
gwenmich <79330147+cgmich@users.noreply.github.com>
2024-12-08 11:04:02 +0000

    Deleted all gitkeep files

--- handlers/hitboxes.py
+++ handlers/hitboxes.py
@@ -3,6 +3,20 @@ import pygame
 # Initializing
 pygame.init()
 
+# pass where you are bliting and the coordinates
+def hitbox_visible_square(surface, x, y, w, h):
+    pygame.init()
+    building = pygame.Rect(x, y, w, h)
+    # give it transparency
+    transparent_surface = pygame.Surface((building.width, building.height), pygame.SRCALPHA)
+    # 128 is the alpha value
+    transparent_surface.fill((255, 0, 0, 128))
+
+    # Blit the semi-transparent hitbox onto the screen
+    surface.blit(transparent_surface, (building.x, building.y))
+
+
+
 # hitboxes for buildings to determine collisions and create walls
 # Rect creates a rectangle at the set coordinates with the specified dimensions
 
